6d4cb91a2ac467bc979b02ec15cfbad4f997955a
Eino Juhani Oltedal <eino.juhani.oltedal@cern.ch>
2014-07-29 11:36:13 +0200

    Completed the add event function

--- challenge1/intermediate/intermediate.c
+++ challenge1/intermediate/intermediate.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-struct event {
-    char description[141]; // max description length is 140 + termination 0
-    char date[17]; //example 2014-06-02 16:00 must be 16 chars + termination 0
-        struct event *next;
-};
 
-int addEvent(char description[141], char date[17], struct event *root)
+typedef struct e {
+    char *description; // max description length is 140 + termination 0
+    char *date; //example 2014-06-02 16:00 must be 16 chars + termination 0
+    struct e *next;
+} event;
+
+
+int addEvent(char *description, char *date, event **root)
 {
     if (strlen(description) > 140) {
         return 1; // desciption too long
@@ -14,21 +17,61 @@ int addEvent(char description[141], char date[17], struct event *root)
         return 2; // date not the correct format
     }
 
-    // user recursion to add to the end of the list
-    if (root->date == 0) {
-            root->date = date[0];
-        root->description = description
-    } else { 
-        addEvent(description, date, root);
+    /*if the root is empty, add at root*/
+    if (*root == 0) {
+        printf("test added root\n");
+        *root = malloc(sizeof(event));
+        (*root)->date = date;
+        (*root)->description = description;
+        (*root)->next = 0;
+    } else {
+        event *tmp;
+        tmp = *root;
+        while (tmp->next != 0)
+        {
+            tmp = tmp->next;
+        }
+        printf("adding new event\n");
+        tmp->next = malloc(sizeof(event));   
+        tmp = tmp->next;
+        tmp->description = description;
+        tmp->date = date;
     }
     return 0;
 }
 
+int removeEvent(char *date) {
+
+}
+
+void printEvents(event *root)
+{
+    event *tmp;
+    tmp = root;
+
+    while (tmp != 0)
+    {
+        printf("%s %s\n", tmp->date, tmp->description);
+        tmp = tmp->next;
+    }
+}
+
 int main(void)
 {
-    struct event *root;
 
-    printf("%d\n", root);
+    event *root;
+    char *d1, *d2, *t1, *t2;
+    d1 = malloc(141*sizeof(char));
+    d2 = malloc(141*sizeof(char));
+    t1 = malloc(17*sizeof(char));
+    t2 = malloc(17*sizeof(char));
+    d1 = "Bursdag";
+    d2 = "Meeting";
+    t1 = "2014-11-01 15:00";
+    t2 = "2015-02-11 10:00";
+    addEvent(d1,t1,&root);
+    addEvent(d2,t2,&root);
+    printEvents(root);
 
     return 0;
 }
