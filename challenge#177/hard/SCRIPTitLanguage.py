9cc3dd417efca7c8270941cdfa33d1755eb24e40
eino <einojo@karhu.workspace>
2014-08-29 22:58:16 +0200

    challenge#177

---
+++ challenge#177/hard/SCRIPTitLanguage.py
@@ -0,0 +1,7 @@
+class Scene():
+    def __init__(self, words, topWords, characters, directions):
+        self.words = words
+        self.topWords = topWords
+        self.characters = characters
+        self.directions = directions
+
